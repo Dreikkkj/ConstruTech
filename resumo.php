669d91b017f531707fafa8a03ca9c92ceb6fd7a4
Dreikkj <andreiluna71@gmail.com>
2026-04-08 17:04:47 -0300

    updated

--- resumo.php
+++ resumo.php
@@ -9,8 +9,32 @@
     <title>Resumo Financeiro</title>
     <link rel="stylesheet" href="andreigeral.css">
 </head>
-
 <body>
+    <div class="conteiner-box">
+        <h2>Resumo Financeiro</h2>
+        <p>Visão geral do valor investido no estoque.</p>
+    </div>
+    <div class="conteiner-valores">
+        <div class="valor-estoque">
+            <h3>Valor Total em Estoque</h3>
+<!--Colocar php aqui no valor-->
+            <p>R$ 10.000,00</p>
+        </div>
+        <div class="total-unidades">
+            <h3>Valor Médio por Item</h3>
+            <p>R$ 500,00</p>
+        </div>
+        <div class="produtos-cadastrados">
+            <h3>Valor do Estoque Atual</h3>
+            <p>R$ 8.000,00</p>
+        </div>
+    </div>
+    <div class="valor-categoria">
+        <h3>Valor por Categoria</h3>
+        <p>R$ 4.000,00 - Materiais de Construção</p>
+        <p>R$ 2.000,00 - Ferrame
+    </div>
+
 </body>
 
 </html>
