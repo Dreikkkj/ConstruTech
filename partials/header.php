8da5d127e13e1fadbb582ae75d61c2094887aee3
Dreikkj <andreiluna71@gmail.com>
2026-04-09 16:08:42 -0300

    header.php updated

--- partials/header.php
+++ partials/header.php
@@ -6,11 +6,11 @@
                 <h1 class="font-cursiva">ConstruTech</h1>
             </div>
             <ul>
-                <li><a href="./cadastro.php">Cadastro</a></li>
-                <li><a href="./inventario.php">Inventário</a></li>
-                <li><a href="./alertas.php">Alertas</a></li>
-                <li><a href="./movimentacao.php">Movimentação</a></li>
-                <li><a href="./resumo.php">Resumo</a></li>
+                <li><a href="./cadastro.php" <?php if(basename($_SERVER['PHP_SELF']) == 'cadastro.php') echo 'class="active"'; ?>>Cadastro</a></li>
+                <li><a href="./inventario.php" <?php if(basename($_SERVER['PHP_SELF']) == 'inventario.php') echo 'class="active"'; ?>>Inventário</a></li>
+                <li><a href="./alertas.php" <?php if(basename($_SERVER['PHP_SELF']) == 'alertas.php') echo 'class="active"'; ?>>Alertas</a></li>
+                <li><a href="./movimentacao.php" <?php if(basename($_SERVER['PHP_SELF']) == 'movimentacao.php') echo 'class="active"'; ?>>Movimentação</a></li>
+                <li><a href="./resumo.php" <?php if(basename($_SERVER['PHP_SELF']) == 'resumo.php') echo 'class="active"'; ?>>Resumo</a></li>
             </ul>
         </ul>
     </nav>
