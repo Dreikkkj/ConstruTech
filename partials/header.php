43b8c6866784785b4adea15127e52ba339c04386
Dreikkj <andreiluna71@gmail.com>
2026-04-09 15:18:21 -0300

    Header done.

--- partials/header.php
+++ partials/header.php
@@ -1,1 +1,17 @@
-<!-- oi -->
+<header>
+    <nav>
+        <ul class="menu-superior">
+            <div class="logo">
+                <img src="./fotos/logocontrutech1.png" alt="Logo da Loja">
+                <h1 class="font-cursiva">ConstruTech</h1>
+            </div>
+            <ul>
+                <li><a href="./cadastro.php">Cadastro</a></li>
+                <li><a href="./inventario.php">Inventário</a></li>
+                <li><a href="./alertas.php">Alertas</a></li>
+                <li><a href="./movimentacao.php">Movimentação</a></li>
+                <li><a href="./resumo.php">Resumo</a></li>
+            </ul>
+        </ul>
+    </nav>
+</header>
--- resumo.php
+++ resumo.php
@@ -7,9 +7,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Resumo Financeiro</title>
-    <link rel="stylesheet" href="andreigeral.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <!-- Fontes -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Google+Sans:ital,opsz,wght@0,17..18,400..700;1,17..18,400..700&family=Lexend+Deca:wght@100..900&display=swap"
+        rel="stylesheet">
 </head>
+
 <body>
+    <?php
+    require_once './partials/header.php';
+    ?>
+    <br><br><br><br><br><br><br>
     <div class="conteiner-box">
         <h2>Resumo Financeiro</h2>
         <p>Visão geral do valor investido no estoque.</p>
@@ -17,7 +28,7 @@
     <div class="conteiner-valores">
         <div class="valor-estoque">
             <h3>Valor Total em Estoque</h3>
-<!--Colocar php aqui no valor-->
+            <!--Colocar php aqui no valor-->
             <p>R$ 10.000,00</p>
         </div>
         <div class="total-unidades">
