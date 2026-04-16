7f9284b011b3e067bb72249623f06f84682b1d5d
Dreikkj <andreiluna71@gmail.com>
2026-04-16 16:42:30 -0300

    .

--- movimentacao.php
+++ movimentacao.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Movimentação</title>
-    
+    <!-- fontes -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Google+Sans:ital,opsz,wght@0,17..18,400..700;1,17..18,400..700&family=Lexend+Deca:wght@100..900&display=swap" rel="stylesheet">
@@ -22,7 +22,14 @@
         <div class="conteiner-box">
             <div class="maintitle">
                 <h2 class="mov">Movimentação</h2>
-                <p class="mov">Registre as entradas e saídas de produtos no estoque.</p>
+                <p class="mov">Adicione ou remova unidades de produtos existentes.</p>
+            </div>
+            <div class="cimento">
+                <h3><img src="./fotos/imagecadastro.png" alt="Cimento CP-II 5-kg"> CP-II 5-kg</h3>
+                <p>categoria</p>
+                <P class="qntd-estoque">em estoque</P>
+                <button class="add">Adicionar</button>
+                <button class="remove">Remover</button>
             </div>
         </div>
     </body>
